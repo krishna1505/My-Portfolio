c5453469aa7eeab7f82922265b48d9af2fa2443d
Krishna Mohan Yadav <145401206+krishna1505@users.noreply.github.com>
2024-06-30 11:49:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+
+
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks  = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height =sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' +id + ']').classList.add('active');
+            });
+        };
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky' , window.scrollY >100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+}
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content , heading', { origin:'top' });
+ScrollReveal().reveal('.home-img , .skills-content, .project-box, .contact form,.footer-content', {origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img , .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content' , { origin:'right' });
+
+// var typed6 = new Typed ('#typed6',{
+//     Strings: ['Web Developer' , 'Python Developer', 'Engineer'],
+//     typeSpeed: 70,
+//     backSpeed: 70,
+//     backDelay: 1000,
+//     loop: true,
+// });
+
+
+
+var typed6 = new Typed('#typed6', {
+    strings: ['Web Developer' , 'Python Developer','Fronted Developer', 'Engineer'],
+   typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+  });
+
+  var typed7 = new Typed('#typed7', {
+    strings: ['Web Developer' , 'Python Developer','Fronted Developer', 'Engineer'],
+   typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+  });
